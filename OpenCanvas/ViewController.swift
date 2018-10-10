f9f52d9512cd1449efc70b47c78f97184216d3e2
Victor Li <victorli5674@yahoo.com>
2018-10-09 22:29:00 -0700

    init new project

---
+++ OpenCanvas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenCanvas
+//
+//  Created by Victor Li on 10/9/18.
+//  Copyright © 2018 Victor Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
